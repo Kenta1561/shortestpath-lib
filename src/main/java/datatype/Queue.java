f4969e4656078e7f8ac100dae91d115ddfe22f6e
Kenta1561 <kentakariya@icloud.com>
2017-02-16 18:08:39 +0100

    Changed Queue class

--- src/main/java/datatype/Queue.java
+++ src/main/java/datatype/Queue.java
@@ -1,7 +1,35 @@
 package main.java.datatype;
 
-/**
- * Created by kenta on 16.02.2017.
- */
+import java.util.ArrayList;
+
 public class Queue {
+
+    private final ArrayList<Node> queue;
+
+    public Queue() {
+        queue = new ArrayList<>();
+    }
+
+    public void addNode(Node node) {
+        queue.add(node);
+        sortQueue();
+    }
+
+    public void removeFirstNode() {
+        queue.remove(0);
+        sortQueue();
+    }
+
+    public Node getFirstNode() {
+        return queue.get(0);
+    }
+
+    public int getQueueLength() {
+        return queue.size();
+    }
+
+    private void sortQueue() {
+        queue.sort(Node::compareTo);
+    }
+
 }
