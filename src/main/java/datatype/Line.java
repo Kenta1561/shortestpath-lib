b26df7ed123d25ef70506b245fe93a23f17b2c12
Kenta1561 <kentakariya@icloud.com>
2017-02-16 18:03:32 +0100

    Changed Line class

--- src/main/java/datatype/Line.java
+++ src/main/java/datatype/Line.java
@@ -1,7 +1,23 @@
 package main.java.datatype;
 
-/**
- * Created by kenta on 16.02.2017.
- */
+import javafx.scene.paint.Paint;
+
 public class Line {
+
+    private final String name;
+    private final Paint color;
+
+    public Line(String name, Paint color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Paint getColor() {
+        return color;
+    }
+
 }
