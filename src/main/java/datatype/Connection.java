cfaed5023a7987d32a37413e44a99c9c18b66fd3
Kenta1561 <kentakariya@icloud.com>
2017-02-16 17:54:53 +0100

    Added Node class, changed Connection class

--- src/main/java/datatype/Connection.java
+++ src/main/java/datatype/Connection.java
@@ -1,7 +1,27 @@
 package main.java.datatype;
 
-/**
- * Created by kenta on 16.02.2017.
- */
 public class Connection {
+
+    private Node from;
+    private Node to;
+    private double rawTime;
+
+    public Connection(Node from, Node to, double rawTime) {
+        this.from = from;
+        this.to = to;
+        this.rawTime = rawTime;
+    }
+
+    public Node getFrom() {
+        return from;
+    }
+
+    public Node getTo() {
+        return to;
+    }
+
+    public double getRawTime() {
+        return rawTime;
+    }
+    
 }
---
+++ src/main/java/datatype/Node.java
@@ -0,0 +1,7 @@
+package main.java.datatype;
+
+/**
+ * Created by kenta on 16.02.2017.
+ */
+public class Node {
+}
