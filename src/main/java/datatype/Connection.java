b21ebd06eb112cbe1506cb4dae7dcaa401f6a8b6
Kenta1561 <kentakariya@icloud.com>
2017-02-16 17:59:59 +0100

    Changed Connection class and Node class

--- src/main/java/datatype/Connection.java
+++ src/main/java/datatype/Connection.java
@@ -23,5 +23,5 @@ public class Connection {
     public double getRawTime() {
         return rawTime;
     }
-    
+
 }
--- src/main/java/datatype/Node.java
+++ src/main/java/datatype/Node.java
@@ -1,7 +1,60 @@
 package main.java.datatype;
 
-/**
- * Created by kenta on 16.02.2017.
- */
-public class Node {
+import java.util.ArrayList;
+
+public class Node implements Comparable<Node> {
+
+    private final double code;
+    private final String name;
+    private double time;
+    private final ArrayList<Line> lines;
+    private Node previous;
+
+    public Node(double code, String name, double time, ArrayList<Line> lines, Node previous) {
+        this.code = code;
+        this.name = name;
+        this.time = time;
+        this.lines = lines;
+        this.previous = previous;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+
+    public double getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public ArrayList<Line> getLines() {
+        return lines;
+    }
+
+    public Node getPrevious() {
+        return previous;
+    }
+
+    @Override
+    public int compareTo(Node node) {
+        if(time < node.getTime()) {
+            return -1;
+        } else if(time > node.getTime()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
 }
