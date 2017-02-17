694a6fb0bbc8838c97d7078dd82bda0d39b0cd92
Kenta1561 <kentakariya@icloud.com>
2017-02-17 22:16:18 +0100

    Changed access level of Path class

--- src/main/java/datatype/Path.java
+++ src/main/java/datatype/Path.java
@@ -1,11 +1,11 @@
 package main.java.datatype;
 
-class Path {
+public class Path {
 
     private final Line line;
     private final Node to;
 
-    Path(Line line, Node to) {
+    public Path(Line line, Node to) {
         this.line = line;
         this.to = to;
     }
