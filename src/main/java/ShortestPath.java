5da0c70bbfc572f2e17cd92ee1995af4e1e5b984
Kenta1561 <kentakariya@icloud.com>
2017-02-17 21:37:48 +0100

    Changed from ArrayList to HashSet for storing instances of Node class and Line class

--- src/main/java/ShortestPath.java
+++ src/main/java/ShortestPath.java
@@ -2,8 +2,9 @@ package main.java;
 
 import main.java.datatype.Line;
 import main.java.datatype.Node;
+import main.java.exception.ItemDuplicationException;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  * This is the main class of this library. An instance of this class is
@@ -14,43 +15,65 @@ public class ShortestPath {
     /**
      * This ArrayList stores all nodes in a network
      */
-    public final ArrayList<Node> nodes;
+    public final HashSet<Node> nodes;
 
     /**
      * This ArrayList stores all lines in a network
      */
-    public final ArrayList<Line> lines;
+    public final HashSet<Line> lines;
 
     /**
      * Basic constructor, initializes both ArrayLists
      */
     public ShortestPath() {
-        nodes = new ArrayList<>();
-        lines = new ArrayList<>();
+        nodes = new HashSet<>();
+        lines = new HashSet<>();
     }
 
     /**
      * Add a line in the network
      * @param line Requires a {@link Line} object
      */
-    public void addLine(Line line) {}
+    public void registerLine(Line line) {
+        if(!lines.add(line)) {
+            throw new ItemDuplicationException("You have already registered this instance of Line.");
+        }
+    }
 
     /**
      * Add lines in the network
-     * @param lines Requires an ArrayList with {@link Line} objects
+     * @param lines Requires a HashSet with {@link Line} objects
      */
-    public void addLines(ArrayList<Line> lines) {}
+    public void registerLines(HashSet<Line> lines) {
+        for(Line line : lines) {
+            if(!this.lines.add(line)) {
+                throw new ItemDuplicationException("You have already registered one or more instances of Line in the " +
+                        "given HashSet.");
+            }
+        }
+    }
 
     /**
      * Add a node in the network
      * @param node Requires a {@link Node} object
      */
-    public void addNode(Node node) {}
+    public void registerNode(Node node) {
+        if(!nodes.add(node)) {
+            throw new ItemDuplicationException("You have already registered this instance of Node.");
+        }
+    }
 
     /**
      * Add nodes in the network
-     * @param nodes Requires an ArrayList with {@link Node} objects
+     * @param nodes Requires a HashSet with {@link Node} objects
      */
-    public void addNodes(ArrayList<Node> nodes) {}
+    public void registerNodes(HashSet<Node> nodes) {
+        for(Node node : nodes) {
+            if(!this.nodes.add(node)) {
+                throw new ItemDuplicationException("You have already registered one or more instances of Node in the " +
+                        "given HashSet.");
+            }
+        }
+    }
 
 }
