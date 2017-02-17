3358c6607803e555ceefd44c88acb90935bdd74d
Kenta1561 <kentakariya@icloud.com>
2017-02-17 21:13:40 +0100

    Added JavaDoc for ShortestPath class, changed JavaDoc files

--- src/main/java/ShortestPath.java
+++ src/main/java/ShortestPath.java
@@ -11,20 +11,46 @@ import java.util.ArrayList;
  */
 public class ShortestPath {
 
+    /**
+     * This ArrayList stores all nodes in a network
+     */
     public final ArrayList<Node> nodes;
+
+    /**
+     * This ArrayList stores all lines in a network
+     */
     public final ArrayList<Line> lines;
 
+    /**
+     * Basic constructor, initializes both ArrayLists
+     */
     public ShortestPath() {
         nodes = new ArrayList<>();
         lines = new ArrayList<>();
     }
 
+    /**
+     * Add a line in the network
+     * @param line Requires a {@link Line} object
+     */
     public void addLine(Line line) {}
 
+    /**
+     * Add lines in the network
+     * @param lines Requires an ArrayList with {@link Line} objects
+     */
     public void addLines(ArrayList<Line> lines) {}
 
+    /**
+     * Add a node in the network
+     * @param node Requires a {@link Node} object
+     */
     public void addNode(Node node) {}
 
+    /**
+     * Add nodes in the network
+     * @param nodes Requires an ArrayList with {@link Node} objects
+     */
     public void addNodes(ArrayList<Node> nodes) {}
 
 }
