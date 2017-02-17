b900fb70b43c9e22b4e7906e0035df981f8507f3
Kenta1561 <kentakariya@icloud.com>
2017-02-17 22:18:34 +0100

    Added content to Queue class in main package

--- src/main/java/Queue.java
+++ src/main/java/Queue.java
@@ -1,7 +1,37 @@
 package main.java;
 
-/**
- * Created by kenta on 16.02.2017.
- */
-public class Queue {
+import main.java.datatype.Node;
+
+import java.util.ArrayList;
+
+class Queue {
+
+    private final ArrayList<Node> queue;
+
+    Queue() {
+        queue = new ArrayList<>();
+    }
+
+    void addNode(Node node) {
+        queue.add(node);
+        sortQueue();
+    }
+
+    void removeFirstNode() {
+        queue.remove(0);
+        sortQueue();
+    }
+
+    Node getFirstNode() {
+        return queue.get(0);
+    }
+
+    int getQueueLength() {
+        return queue.size();
+    }
+
+    private void sortQueue() {
+        queue.sort(Node::compareTo);
+    }
+
 }
