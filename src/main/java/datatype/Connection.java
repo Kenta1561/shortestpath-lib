c17149058832eebb7a79e028472f69c0d49c3adb
Kenta1561 <kentakariya@icloud.com>
2017-02-19 17:37:07 +0100

    Added JavaDoc comments in Connection class

--- src/main/java/datatype/Connection.java
+++ src/main/java/datatype/Connection.java
@@ -1,11 +1,31 @@
 package main.java.datatype;
 
+/**
+ * Objects of this class represent a connection from one node to another.
+ */
 public class Connection {
 
+    /**
+     * The origin node
+     */
     private final Node from;
+
+    /**
+     * The destination node
+     */
     private final Node to;
+
+    /**
+     * The raw travel time between the two nodes
+     */
     private final double rawTime;
 
+    /**
+     * Basic constructor with all parameters given
+     * @param from Requires the origin node
+     * @param to Requires the destinatio node
+     * @param rawTime Requires the raw travel time between the two nodes
+     */
     public Connection(Node from, Node to, double rawTime) {
         this.from = from;
         this.to = to;
@@ -13,18 +33,33 @@ public class Connection {
         setConnectivityValidation();
     }
 
+    /**
+     * Getter for origin node
+     * @return Returns the origin node
+     */
     public Node getFrom() {
         return from;
     }
 
+    /**
+     * Getter for destination node
+     * @return Returns the destination node
+     */
     public Node getTo() {
         return to;
     }
 
+    /**
+     * Getter for raw travel time
+     * @return Returns the raw travel time
+     */
     public double getRawTime() {
         return rawTime;
     }
 
+    /**
+     * Private method for validate connectivity of both nodes
+     */
     private void setConnectivityValidation() {
         from.validateConnectivity();
         to.validateConnectivity();
