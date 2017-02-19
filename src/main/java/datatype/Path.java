f1c6815c1a81d6e9d0d84271ec29402ae60c495a
Kenta1561 <kentakariya@icloud.com>
2017-02-19 17:43:45 +0100

    Added variable from as an object of Node class that represents the origin node, added JavaDoc comments

--- src/main/java/datatype/Path.java
+++ src/main/java/datatype/Path.java
@@ -1,19 +1,58 @@
 package main.java.datatype;
 
+/**
+ * @author Kenta1561
+ * Objects of this class represent a section of a path from point A to B.
+ */
 public class Path {
 
+    /**
+     * The Line that must be taken
+     */
     private final Line line;
+
+    /**
+     * The origin node
+     */
+    private final Node from;
+
+    /**
+     * The destination node
+     */
     private final Node to;
 
-    public Path(Line line, Node to) {
+    /**
+     * Basic constructor with all parameters
+     * @param line Requires the line that must be taken
+     * @param from Requires the origin node
+     * @param to Requires the destination node
+     */
+    public Path(Line line, Node from, Node to) {
         this.line = line;
+        this.from = from;
         this.to = to;
     }
 
+    /**
+     * Getter for the line that must be taken
+     * @return Returns the line
+     */
     public Line getLine() {
         return line;
     }
 
+    /**
+     * Getter for the origin node
+     * @return Returns the origin node
+     */
+    public Node getFrom() {
+        return from;
+    }
+
+    /**
+     * Getter for the destination node
+     * @return Returns the destination node
+     */
     public Node getTo() {
         return to;
     }
