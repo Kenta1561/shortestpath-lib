4e9eed623768bb582abd018c44505649463f3f1b
Kenta1561 <kentakariya@icloud.com>
2017-02-19 17:28:09 +0100

    Added JavaDoc to Queue class and removed unnecessary method

--- src/main/java/Queue.java
+++ src/main/java/Queue.java
@@ -4,32 +4,51 @@ import main.java.datatype.Node;
 
 import java.util.ArrayList;
 
+/**
+ * This class manages the queue that stores nodes awaiting for being processed.
+ */
 class Queue {
 
+    /**
+     * The queue with the nodes
+     */
     private final ArrayList<Node> queue;
 
+    /**
+     * Basic constructor, initializes the queue ArrayList
+     */
     Queue() {
         queue = new ArrayList<>();
     }
 
+    /**
+     * Adds a node to the queue and sorts it at the same time
+     * @param node Requires the node to be added
+     */
     void addNode(Node node) {
         queue.add(node);
         sortQueue();
     }
 
+    /**
+     * Remove the first node from the queue and sorts it at the same time
+     */
     void removeFirstNode() {
         queue.remove(0);
         sortQueue();
     }
 
+    /**
+     * Returns the first {@link Node} instance from the queue
+     * @return Returns the first {@link Node} instance from the queue
+     */
     Node getFirstNode() {
         return queue.get(0);
     }
 
-    int getQueueLength() {
-        return queue.size();
-    }
-
+    /**
+     * Sorts the queue by using {@link Node#compareTo(Node)} (sorts by travel time from starting node)
+     */
     void sortQueue() {
         queue.sort(Node::compareTo);
     }
