2421e3727ac0eb174a92996aaac248936c9556d1
Kenta1561 <kentakariya@icloud.com>
2017-02-19 17:29:49 +0100

    Added new exception MultipleRegistrationException

---
+++ src/main/java/exception/MultipleRegistrationException.java
@@ -0,0 +1,18 @@
+package main.java.exception;
+
+/**
+ * This exception gets thrown when a connection with the same origin and destination like another connection
+ * was added to the HashSet. For the exception that gets thrown when the same instance of
+ * {@link main.java.datatype.Connection} was added, see {@link ItemDuplicationException}.
+ */
+public class MultipleRegistrationException extends RuntimeException {
+
+    /**
+     * Constructor with String as exception message
+     * @param message Requires a String as the message
+     */
+    public MultipleRegistrationException(String message) {
+        super(message);
+    }
+
+}
