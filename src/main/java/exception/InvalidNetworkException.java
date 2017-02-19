6d5c7edb5856aa73a12707665623d5bd49774eba
Kenta1561 <kenta1561@yahoo.de>
2017-02-19 17:38:35 +0100

    Added JavaDoc comments in InvalidNetworkException class

--- src/main/java/exception/InvalidNetworkException.java
+++ src/main/java/exception/InvalidNetworkException.java
@@ -1,10 +1,14 @@
 package main.java.exception;
 
 /**
- * Created by kenta on 18.02.2017.
+ * This exception gets thrown when the provided network was invalid due to lack of connectivity between nodes.
  */
 public class InvalidNetworkException extends RuntimeException {
 
+    /**
+     * Constructor with String as exception message
+     * @param message Requires a String as the message
+     */
     public InvalidNetworkException(String message) {
         super(message);
     }
